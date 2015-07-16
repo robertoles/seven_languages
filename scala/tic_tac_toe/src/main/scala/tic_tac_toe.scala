eda9f08c646e2a04c66a346f623ae024bf4573c4
robertoles <robertoles@me.com>
2015-07-16 20:26:14 +0100

    Refactoring

--- scala/tic_tac_toe/src/main/scala/tic_tac_toe.scala
+++ scala/tic_tac_toe/src/main/scala/tic_tac_toe.scala
@@ -1,38 +1,98 @@
 package tictactoe
 
+class Cell {
+  
+  var player = ""
+  var index = 0
+
+  def this(index: Int) = {
+    this();
+    this.index = index;
+  }
+
+  def indexMatch(otherIndex: Integer):Boolean = {
+    index == otherIndex
+  }
+
+  def claim(claimingPlayer: String):Boolean = {
+    if (claimed) { return false }
+    player = claimingPlayer
+    true
+  }
+
+  def claimed(): Boolean = {
+    player != ""
+  }
+
+  def unclaimed(): Boolean = {
+    !claimed();
+  }
+
+  def claimedBy(targetPlayer: String):Boolean = { player == targetPlayer }
+
+  def row():Int = { ((index-1) / 3).floor.toInt }
+}
+
 class TicTacToe {
 
-  var board = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
+  var board = (1 to 9).map { x => new Cell(x) }
   var player = "X"
   var winner = ""
 
   def start() {
   }
 
-  def draw() = {
-    List("1 2 3", "4 5 6", "7 8 9")
-  }
-
   def claim(selection:Int):Boolean = {
-    var unclaimed_count = board.count { _  == selection }
-    if (unclaimed_count == 0) { return false }
+    if (cellClaimed(selection)) { return false }
 
     // Take the cell
-    var player_number = if (player == "X") 10 else 11
-    board = board.map { case `selection` => player_number; case x => x }
+    board.find { x => x.indexMatch(selection) }.get.claim(player)
     
-    // check if first row is a winner
-    var rows = board
-      .map{ case x => if (x == player_number) true else false }
-      .grouped(3)
+    // check if row is a winner
+    
 
-    var game_over = rows.map { _ == List(true,true,true) }.filter{ _ == true }.length > 0
+    if (isWinningRow) {
+      winner = player
+    }
+
+    /*
+    // check if column is a winner
+    var columns = board
+      .map{ case x => if (x == player_number) true else false }
+      .groupedBy { _ % 3 }
+
+    game_over = (columns.map { _ == List(true,true,true) }.filter{ _ == true }.length > 0)
     if (game_over) {
       winner = player
     }
 
     // switch user
-    player = if (player == "X") "O" else "X"
+    
+    */
+
+    switchPlayer()
     return true
+  }
+
+  def cellClaimed(cell_number:Int):Boolean = {
+    getCell(cell_number).claimed
+  }
+
+  def getCell(cell_number: Int):Cell = {
+    board.find { x => x.indexMatch(cell_number) }.get
+  }
+
+  def switchPlayer() = {
+    player = if (player == "X") "O" else "X"
+  }
+
+  def isWinningRow():Boolean = {
+    board
+      .groupBy { _.row }
+      .exists { 
+        row => row._2.forall { 
+          cell => cell.claimedBy(player) 
+      } 
+    }
   }
 }
--- scala/tic_tac_toe/src/test/scala/tic_tac_toe_suite.scala
+++ scala/tic_tac_toe/src/test/scala/tic_tac_toe_suite.scala
@@ -3,34 +3,30 @@ import tictactoe._
 
 class TicTacToeSuite extends FunSuite {
 
-    test("Starting a game displays welcome message") {
-        var game = new TicTacToe()
-        game.start()
-        var board = game.draw()
-        assert(board.indexOf("1 2 3") === 0)
-        assert(board.indexOf("4 5 6") === 1)
-        assert(board.indexOf("7 8 9") === 2)
-    }
-
-    test("Claiming an unselected cell with X the cell number is replaced with X=10") {
+    test("Claiming an unselected cell with X the cell number sets the cell player to X") {
         var game = new TicTacToe()
         game.player = "X"
         game.claim(1)
-        assert(game.board === List(10, 2, 3, 4, 5, 6, 7, 8, 9))
+        var cell = game.getCell(1)
+        assert(cell.claimed == true)
+        assert(cell.player == "X")
+        (2 to 9).foreach { x => assert(game.cellClaimed(x) === false) }
     }
 
-    test("Claiming an unselected cell with O the cell number is replaced with O=11") {
+    test("Claiming an unselected cell with O the cell number sets the cell player to O") {
         var game = new TicTacToe()
         game.player = "O"
         game.claim(2)
-        assert(game.board === List(1, 11, 3, 4, 5, 6, 7, 8, 9))
+        var cell = game.getCell(2)
+        assert(cell.claimed == true)
+        assert(cell.player == "O")
     }
 
     test("Claiming an unselected cell which doesnt result in a win doensnt set the winner") {
         var game = new TicTacToe()
-        game.board = List(1, 2, 3, 4, 5, 6, 7, 10, 9)
         game.player = "X"
         assert(game.winner === "")
+        game.claim(8)
         game.claim(9)
         assert(game.winner === "")
     }
@@ -42,6 +38,7 @@ class TicTacToeSuite extends FunSuite {
         assert(game.player === "O")
     }
 
+    
     test("When current player is O, claiming an unselected cell switches current player to X") {
         var game = new TicTacToe()
         game.player = "O"
@@ -49,25 +46,36 @@ class TicTacToeSuite extends FunSuite {
         assert(game.player === "X")
     }
 
+
     test("When claiming a selected cell the board doesnt change") {
         var game = new TicTacToe()
-        game.board = List(10, 2, 3, 4, 5, 6, 7, 8, 9)
+        game.player = "X"
+        game.claim(1)
         game.player = "O"
         game.claim(1)
-        assert(game.board === List(10, 2, 3, 4, 5, 6, 7, 8, 9))
+        var cell = game.getCell(1)
+        assert(cell.claimed == true)
+        assert(cell.player == "X")
+        assert(game.player == "O")
     }
 
+    
     test("When claiming a selected cell the current player doesnt change") {
         var game = new TicTacToe()
-        game.board = List(10, 2, 3, 4, 5, 6, 7, 8, 9)
+        game.player = "O"
+        game.claim(1)
         game.player = "O"
         game.claim(1)
         assert(game.player === "O")
     }
 
+    
     test("When X claims the top row the winner is set to X") {
         var game = new TicTacToe()
-        game.board = List(10, 10, 3, 4, 5, 6, 7, 8, 9)
+        game.player = "X"
+        game.claim(1)
+        game.player = "X"
+        game.claim(2)
         game.player = "X"
         assert(game.winner === "")
         game.claim(3)
@@ -76,7 +84,10 @@ class TicTacToeSuite extends FunSuite {
 
     test("When O claims the top row the winner is set to O") {
         var game = new TicTacToe()
-        game.board = List(11, 11, 3, 4, 5, 6, 7, 8, 9)
+        game.player = "O"
+        game.claim(1)
+        game.player = "O"
+        game.claim(2)
         game.player = "O"
         assert(game.winner === "")
         game.claim(3)
@@ -85,7 +96,10 @@ class TicTacToeSuite extends FunSuite {
 
     test("When X claims the middle row the winner is set to X") {
         var game = new TicTacToe()
-        game.board = List(1, 2, 3, 10, 10, 6, 7, 8, 9)
+        game.player = "X"
+        game.claim(4)
+        game.player = "X"
+        game.claim(5)
         game.player = "X"
         assert(game.winner === "")
         game.claim(6)
@@ -94,11 +108,23 @@ class TicTacToeSuite extends FunSuite {
 
     test("When X claims the bottom row the winner is set to X") {
         var game = new TicTacToe()
-        game.board = List(1, 2, 3, 4, 5, 6, 10, 10, 9)
+        game.player = "X"
+        game.claim(7)
+        game.player = "X"
+        game.claim(8)
         game.player = "X"
         assert(game.winner === "")
         game.claim(9)
         assert(game.winner === "X")
     }
-
+/*
+    test("When X claims the first column the winner is set to X") {
+        var game = new TicTacToe()
+        game.board = List(10, 2, 3, 10, 5, 6, 7, 8, 9)
+        game.player = "X"
+        assert(game.winner === "")
+        game.claim(7)
+        assert(game.winner === "X")
+    }
+*/
 }
