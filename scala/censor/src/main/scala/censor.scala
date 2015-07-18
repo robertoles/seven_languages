ff72b3ef1681034c61196904d0453978cced80f3
robertoles <robertoles@me.com>
2015-07-18 21:49:02 +0100

    Loading words from a file

--- scala/censor/src/main/scala/censor.scala
+++ scala/censor/src/main/scala/censor.scala
@@ -1,4 +1,5 @@
 package censor
+import scala.io.Source
 
 trait Censor {
   var words = Map[String,String]()
@@ -7,7 +8,16 @@ trait Censor {
     this.words = words
   }
 
+  def blacklistFromFile(fileName:String) {
+    this.words = Source.fromFile(fileName).getLines().map{pair => 
+      var result = pair.split(":") 
+      new Tuple2(result(0), result(1))
+    }.toMap
+  }
+
   def replaceWords(sentance:String):String = {
-    this.words.foldLeft(sentance){(result, replacement) => result.replaceAll(replacement._1, replacement._2)}
+    this.words.foldLeft(sentance) { (result, replacement) => 
+      result.replaceAll(replacement._1, replacement._2)
+    }
   }
 }
--- scala/censor/src/test/scala/censor_suite.scala
+++ scala/censor/src/test/scala/censor_suite.scala
@@ -1,5 +1,6 @@
 import org.scalatest._
 import censor._
+import java.io.File
 
 class CensorSuite extends FunSuite {
   var words:Map[String,String] = Map()
@@ -15,6 +16,14 @@ class CensorSuite extends FunSuite {
   test("censor with multiple words") {
     val censor = new TestCensor()
     censor.blacklist(Map("Shoot" -> "Plucky", "Darn" -> "Beans"))
+    assert(censor.replaceWords("Darn Shoot me") === "Beans Plucky me")
+  }
+
+  test("censor with words from file") {
+    val censor = new TestCensor()
+    var dir = new File("").getAbsolutePath()
+    var file = new File(dir, "src/test/assets/words.txt").toString()
+    censor.blacklistFromFile(file)
     assert(censor.replaceWords("Darn Shoot me") === "Beans Plucky me")
   }
 }
