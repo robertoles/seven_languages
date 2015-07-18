5d97c22490a725cb87850a7f261ad6bdf8bb83f3
robertoles <robertoles@me.com>
2015-07-18 21:03:43 +0100

    Censor with basic test

--- scala/censor/src/main/scala/censor.scala
+++ scala/censor/src/main/scala/censor.scala
@@ -1,13 +1,13 @@
 package censor
 
 trait Censor {
-  var list = Map[String,String]()
+  var words = Map[String,String]()
 
   def blacklist(words:Map[String,String]) {
-    this.list = words
+    this.words = words
   }
 
   def replaceWords(sentance:String):String = {
-    ""
+    this.words.foldLeft(sentance){(result, replacement) => result.replaceAll(replacement._1, replacement._2)}
   }
 }
