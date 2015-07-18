9607c83eacbf791b3d3b8e3b942483a5784506bb
robertoles <robertoles@me.com>
2015-07-18 21:06:50 +0100

    Adding final test

--- scala/censor/src/test/scala/censor_suite.scala
+++ scala/censor/src/test/scala/censor_suite.scala
@@ -6,11 +6,15 @@ class CensorSuite extends FunSuite {
 
   class TestCensor extends Censor {}
 
-  test("censors single word") {
+  test("censor with single word") {
     val censor = new TestCensor()
     censor.blacklist(Map("Shoot" -> "Plucky"))
     assert(censor.replaceWords("Dont Shoot me") === "Dont Plucky me")
   }
 
-  
+  test("censor with multiple words") {
+    val censor = new TestCensor()
+    censor.blacklist(Map("Shoot" -> "Plucky", "Darn" -> "Beans"))
+    assert(censor.replaceWords("Darn Shoot me") === "Beans Plucky me")
+  }
 }
