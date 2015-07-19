4c7ddb56dc1e073d35c817e32db20404aa4e26e1
robertoles <robertoles@me.com>
2015-07-19 16:45:16 +0100

    Tuple fib

---
+++ haskell/functions/fib_tuple.hs
@@ -0,0 +1,5 @@
+module Main where
+
+  fibTuple :: (Integer, Integer, Integer) -> (Integer, Integer, Integer)
+  fibTuple (x, y, 0) = (x, y, 0)
+  fibTuple (x, y, index) = fibTuple(y, x + y, index - 1)
